71d15666f5176125a982c5a5975ab6de47d03582
wax54 <wax654@gmail.com>
2020-12-15 14:44:38 -0800

    Made a test function with 2 values for calculateMonthPayment function

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,10 +1,12 @@
-
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 600000,rate: 12,years: 1})).toEqual(53309.27);
+  expect(calculateMonthlyPayment({amount: 1000,rate: 36,years: 40})).toEqual(30)
+
 });
 
 
 it("should return a result with 2 decimal places", function() {
+  expect(1).toBe(1);
   // ..
 });
 
