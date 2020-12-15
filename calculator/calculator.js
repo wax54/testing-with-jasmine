d671549adb1a538f2d8d70ec1f43c5b153d5fcab
wax54 <wax654@gmail.com>
2020-12-15 12:51:22 -0800

    filled out update() and updateMonthly()

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -27,28 +27,37 @@ document.getElementById("loan-rate").value = rate;
 }
 
 function setupIntialValues() {
-  
+
   // Put some default values in the inputs
   setCurrentUIValues(600000, 30, 2);
 
   // Call a function to calculate the current monthly payment
-  const res = calculateMonthlyPayment(getCurrentUIValues());
-  console.log(res);
+  update();
+  
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const payment = calculateMonthlyPayment(values);
+  updateMonthly(payment);
+
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  return values.amount;
+
+  return values.amount+'';
 }
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
+/* 
+* Given a string representing the monthly payment value,
+* update the UI to show the value.
+*/
 function updateMonthly(monthly) {
+  const monthlyspan = document.getElementById('monthly-payment');
+  monthlyspan.innerText = monthly; 
 }
