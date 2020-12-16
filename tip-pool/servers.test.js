8500335adf850a54d7b930bc45aca343138cf63a
wax54 <wax654@gmail.com>
2020-12-15 20:33:43 -0800

    Added Tests to server.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -2,16 +2,46 @@ describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
+    submitServerInfo();
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+  describe("submitServerInfo()", function() {
+   
+    it("should be able to handle spaces in server names", function () {
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+      serverNameInput.value = 'Gene Barker';
+      submitServerInfo();
+
+      expect(Object.keys(allServers).length).toEqual(2);
+      expect(allServers['server' + serverId].serverName).toEqual('Gene Barker');
+    });
+
+    it("should add a new server to allServers on submitServerInfo()", function () {
+
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
+    
+    it("should reset the serverNameInput to an empty string", function(){
+
+      expect(serverNameInput.value).toEqual('');
+    });
+  });
+
+  describe("updateServerTable()", function() {
+
+
+    it("should update the serverTbody to include the new server", function(){
+      submitServerInfo();
+
+      expect(serverTbody.innerText).toEqual('Alice	$0.00');
+    });
   });
 
   afterEach(function() {
     // teardown logic
+    allServers = {};
+    serverId = 0;
+    updateServerTable();
   });
 });
