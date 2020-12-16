6677f6dfd03897eeb9a8ecbccc0753d07ab41191
wax54 <wax654@gmail.com>
2020-12-15 21:31:35 -0800

    Added Tests for createCurPayment

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -1,6 +1,6 @@
-describe("payments Test", function() {
+describe("payments Tests", function() {
     
-    describe("submitPaymentInfo tests",function(){
+    describe("submitPaymentInfo Tests",function(){
         beforeEach(function(){
             billAmtInput.value = 60;
             tipAmtInput.value = 5;
@@ -20,16 +20,54 @@ describe("payments Test", function() {
         afterEach(function(){
             paymentTbody.innerText ='';
             allPayments = {};
+            paymentId = 0;
             updateSummary();
         });
     });
-    
+
     describe("createCurPayment Tests", function(){
         beforeEach(function(){
             billAmtInput.value = 60;
             tipAmtInput.value = 5;
         });
-        it("")
+        it("should return a valid payment object",function(){
+            const aPaymentObject = createCurPayment();
+            expect(aPaymentObject.billAmt).toEqual('60');
+            expect(aPaymentObject.tipAmt).toEqual('5');
+            expect(aPaymentObject.tipPercent).toEqual(8);
+        });
+        it("should return undefined with $0 bill",function(){
+            billAmtInput.value = 0;
+            expect(createCurPayment()).toBe(undefined);
+
+        });
+        it("should work with $0 tip",function(){
+            tipAmtInput.value = 0;
+            const aPaymentObject = createCurPayment();
+            expect(aPaymentObject.billAmt).toEqual('60');
+            expect(aPaymentObject.tipAmt).toEqual('0');
+            expect(aPaymentObject.tipPercent).toEqual(0);
+        });
+        it("should return undefined with negative inputs",function(){
+            tipAmtInput.value = -2;
+            expect(createCurPayment()).toBe(undefined);
+            
+            tipAmtInput.value = 4;
+            billAmtInput.value = -2;
+            expect(createCurPayment()).toBe(undefined);
+        });
+        it("should return undefined with empty inputs",function(){
+            tipAmtInput.value = '';
+            expect(createCurPayment()).toBe(undefined);
+
+            tipAmtInput.value = 1;
+            billAmtInput.value = '';
+            expect(createCurPayment()).toBe(undefined);
+        });
+        afterAll(function(){
+            billAmtInput.value = '';
+            tipAmtInput.value = '';
+        });
     });
     
     
